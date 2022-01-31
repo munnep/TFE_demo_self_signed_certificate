a55e36ad0b846cfc1a3fc749f30ce8e155d5c3a0
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2022-01-31 10:53:06 +0100

    Update install_tfe.sh

--- vagrant_scripts/install_tfe.sh
+++ vagrant_scripts/install_tfe.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO
+# copy file to some destination at the computer.
+# set permissions
+
 chmod 600 /vagrant/config/license.rli
 
-curl -o install.sh https://install.terraform.io/ptfe/stable
-bash ./install.sh \
+curl -o /var/tmp/install.sh https://install.terraform.io/ptfe/stable
+
+bash /var/tmp/install.sh \
     no-proxy \
     private-address=192.168.56.33 \
     public-address=192.168.56.33
