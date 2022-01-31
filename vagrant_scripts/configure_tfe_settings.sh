552841501d49b526a6d991d3cf37756b4b9967cd
munnep <patrick.munne@hashicorp.com>
2022-01-31 15:59:21 +0100

    tfe settings in the config location

--- vagrant_scripts/configure_tfe_settings.sh
+++ vagrant_scripts/configure_tfe_settings.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-cat <<-CONF > /vagrant/config/tfe_settings.json
-{
-    "enc_password": {
-        "value": "Password#1"
-    },
-    "hostname": {
-        "value": "192.168.56.33.nip.io"
-    },
-    "installation_type": {
-        "value": "poc"
-    }
-}
-CONF
 
-chmod 600 /vagrant/config/tfe_settings.json
+cp /vagrant/config/tfe_settings.json /var/tmp/tfe_settings.json
+
+chmod 600 /var/tmp/tfe_settings.json
